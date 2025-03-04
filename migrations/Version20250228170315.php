220d90eb95e040cc289abb8880a6e39799ba7cf9
exnet <roa.exnet@gmail.com>
2025-03-04 13:10:25 +0100

    changes

---
+++ migrations/Version20250228170315.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250228170315 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE test');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE test (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, testrel_id INTEGER DEFAULT NULL, CONSTRAINT FK_D87F7E0CCFECEB5A FOREIGN KEY (testrel_id) REFERENCES menu_element (id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D87F7E0CCFECEB5A ON test (testrel_id)');
+    }
+}
