e9d6037ba617564ce2f7cea842e27b1ef2350929
exnet <roa.exnet@gmail.com>
2025-03-18 22:00:48 +0100

    add trustedproxy

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+Request::setTrustedProxies(
+    ['127.0.0.1', 'localhost', '192.168.1.1'],
+    Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO
+);
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
